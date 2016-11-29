81feac9dba2ace595745a5d75c3bb1107929e350
Ganusha93 <mkgjmadusanka@gmail.com>
2016-11-29 19:03:33 +0530

    init

---
+++ cache/com_virtuemart_cats/64a0081114d77277819bda9332ced1cf-cache-com_virtuemart_cats-3bef64624dda3b5695e632509e16d21e.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"output";s:0:"";s:6:"result";a:1:{i:0;O:8:"stdClass":8:{s:22:"virtuemart_category_id";s:2:"28";s:13:"category_name";s:8:"Products";s:20:"category_description";s:20:"<p>All products!</p>";s:8:"metadesc";s:0:"";s:7:"metakey";s:0:"";s:11:"customtitle";s:0:"";s:4:"slug";s:8:"products";s:19:"virtuemart_media_id";a:1:{i:0;s:2:"60";}}}}
